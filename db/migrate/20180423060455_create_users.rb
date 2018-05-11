238813327545f5e9ce3cb12a2dbafb3a127531e7
Amrendra Singh <amrendra@magplus.com>
2018-05-11 12:33:25 +0530

    initial commit

---
+++ db/migrate/20180423060455_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :phone
+      t.string :address
+      t.text :description
+      t.timestamps
+    end
+  end
+end
